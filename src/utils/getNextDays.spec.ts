b86f8e9751f02df9bada417c7f99e05a3f6644e3
BrunoCaputo <brunopc.dev@gmail.com>
2025-08-11 21:50:00 -0300

    test: Group tests with describe

--- src/utils/getNextDays.spec.ts
+++ src/utils/getNextDays.spec.ts
@@ -1,15 +1,17 @@
 import dayjs from "dayjs";
 import { getNextDays } from "./getNextDays";
 
-test("should return the next 5 days in 'DD/MM' format", () => {
-  const days = getNextDays();
-  expect(days).toHaveLength(5);
+describe("getNextDays", () => {
+  it("should return the next 5 days in 'DD/MM' format", () => {
+    const days = getNextDays();
+    expect(days).toHaveLength(5);
 
-  const today = new Date();
-  for (let i = 0; i < 5; i++) {
-    const expectedDate = new Date();
-    expectedDate.setDate(today.getDate() + (i + 1));
-    const expectedFormatted = dayjs(expectedDate).format("DD/MM");
-    expect(days[i]).toBe(expectedFormatted);
-  }
-})
+    const today = new Date();
+    for (let i = 0; i < 5; i++) {
+      const expectedDate = new Date();
+      expectedDate.setDate(today.getDate() + (i + 1));
+      const expectedFormatted = dayjs(expectedDate).format("DD/MM");
+      expect(days[i]).toBe(expectedFormatted);
+    }
+  });
+});
