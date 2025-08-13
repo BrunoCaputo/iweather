450ad7368dada51786bbdb28c52e3c04fa8b958f
BrunoCaputo <brunopc.dev@gmail.com>
2025-08-13 00:00:20 -0300

    test: Select list unit test

---
+++ src/components/SelectList/selectList.spec.tsx
@@ -0,0 +1,18 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+
+import { SelectList } from "@components/SelectList";
+import { CityProps } from "@services/getCityByNameService";
+
+describe("Component: SelectList", () => {
+  it("should return the selected city details", async () => {
+    const data: CityProps[] = [
+      { id: "1", name: "Campinas", latitude: 123, longitude: 456 },
+      { id: "2", name: "Campo grande", latitude: 456, longitude: 123 },
+    ];
+
+    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />);
+
+    const selectedCity = screen.getByText(/campo/i);
+    fireEvent.press(selectedCity);
+  });
+});
