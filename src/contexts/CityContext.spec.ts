d3632dc8932cf105e1e2dd2609d0bbba828e6c27
BrunoCaputo <brunopc.dev@gmail.com>
2025-08-26 23:00:44 -0300

    test: hooks - City Context

---
+++ src/contexts/CityContext.spec.ts
@@ -0,0 +1,20 @@
+import { renderHook, waitFor } from "@testing-library/react-native";
+
+import { useCity } from "@hooks/useCity";
+import { CityProvider } from "./CityContext";
+
+describe("Context: cityContext", () => {
+  it("should change the selected city", async () => {
+    const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
+    await waitFor(() =>
+      result.current.handleChanceCity({
+        id: "1",
+        name: "São Paulo",
+        latitude: 123,
+        longitude: 456,
+      })
+    );
+
+    expect(result.current.city?.name).toBe("São Paulo");
+  });
+});
